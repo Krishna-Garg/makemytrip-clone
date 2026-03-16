bf30d2f1c34b307f966ddfcf915a3c7be1e2433d
Krishna-Garg <krishna.garg.dlp@gmail.com>
2026-03-17 01:34:44 +0530

    Backend Files

---
+++ src/main/java/com/makemytrip/makemytrip/MakemytripApplication.java
@@ -0,0 +1,14 @@
+package com.makemytrip.makemytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class MakemytripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakemytripApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/makemytrip/makemytrip/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.makemytrip.makemytrip.config;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    //Here to remove the springboot deafult login page that repeatedly causes error and cannot be logged in by generated password
+    //If anyone see this, please help
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all endpoints
+                        .allowedOrigins("*") // Allow all origins
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
+                        .allowedHeaders("*") // Allow all headers
+                        .allowCredentials(false); // Disallow credentials for safety
+            }
+        };
+    }
+}
